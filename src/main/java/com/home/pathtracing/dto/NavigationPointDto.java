f4c55e9746b03a25546ab521c35a36a0868b77fd
JyotiparnaMandalPersonal <jyotiparna@hotmail.com>
2025-05-15 10:50:19 +0530

    Removed unnecessary imports

--- src/main/java/com/home/pathtracing/dto/NavigationPointDto.java
+++ src/main/java/com/home/pathtracing/dto/NavigationPointDto.java
@@ -1,13 +1,6 @@
 package com.home.pathtracing.dto;
 
-import jakarta.annotation.Generated;
-import jakarta.persistence.Column;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
